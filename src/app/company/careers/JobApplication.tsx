aee1595bbbeeffaccc1eeba74dc016878f3ab576
Huxleigh Services <service@huxleigh.com>
2025-10-15 23:32:36 +0000

    howing logs for time specified in query. To view more results update you

--- src/app/company/careers/JobApplication.tsx
+++ src/app/company/careers/JobApplication.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useEffect, useActionState } from 'react';
-import { useFormStatus } from 'react-dom';
+import { useEffect } from 'react';
+import { useActionState, useFormStatus } from 'react-dom';
 import { useToast } from '@/hooks/use-toast';
 import { Button } from '@/components/ui/button';
 import {
